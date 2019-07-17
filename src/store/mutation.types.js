400bc2060c478a8b5a919ab16cf4906dfe2cf4dc
vanderkilu <vanderkiliku@gmail.com>
2019-07-17 12:30:01 +0000

    update mutation types

--- src/store/mutation.types.js
+++ src/store/mutation.types.js
@@ -5,4 +5,5 @@ export const SET_USER="fetchUser"
 export const FETCH_START="fetchStart"
 export const FETCH_END="fetchEnd"
 export const SET_ERROR="setError"
+export const SET_ITEM_TYPE="setItemType"
 
