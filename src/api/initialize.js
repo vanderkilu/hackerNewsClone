1cc185c24bfbfcc928129e44ea62d23aacdcf2ff
vanderkilu <vanderkiliku@gmail.com>
2019-07-17 12:27:59 +0000

    rename api directory to service directory

--- src/api/initialize.js
+++
@@ -1,7 +0,0 @@
-import Firebase from 'firebase/app'
-import 'firebase/database'
-
-export function initializeFirebase(params) {
-    Firebase.initializeApp(params.config)
-    return Firebase.database().ref(params.version)
-}
