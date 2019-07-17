3f44a15572b6d8de342f2aefcbff75c58050d72f
vanderkilu <vanderkiliku@gmail.com>
2019-07-17 17:30:48 +0000

    made loader responsive on mobile

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,18 +1,29 @@
 <template>
-    <div class="loader"></div>
+    <div class="loader-container">
+        <div class="loader"></div>
+    </div>
 </template>
 
 <style scoped>
-     .loader {
+    .loader-container {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        background-color: rgba(255,255,255,0.8);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .loader {
         border: 16px solid #f3f3f3; 
         border-top: 16px solid #4a148c; 
         border-radius: 50%;
         width: 120px;
         height: 120px;
         animation: spin 2s linear infinite;
-        position: fixed;
-        top: 50vh;
-        left: 50vw;
+       
     }
 
     @keyframes spin {
