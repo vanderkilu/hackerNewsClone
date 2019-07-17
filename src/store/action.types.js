99be30a124785069f93a91d52f6483a936d4eb4d
vanderkilu <vanderkiliku@gmail.com>
2019-07-17 13:13:58 +0000

    update action types

--- src/store/action.types.js
+++ src/store/action.types.js
@@ -1,4 +1,3 @@
-export const FETCH_POST="fetchPost"
-export const FETCH_COMMENTS="fetchComments"
+export const FETCH_POST_AND_COMMENTS="fetchComments"
 export const FETCH_POSTS="fetchPosts"
 export const FETCH_USER="fetchUser"
