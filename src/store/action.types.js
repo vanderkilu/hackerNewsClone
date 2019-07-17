4f427f8f220b048cabda8b9a81801f6534e0049c
vanderkilu <vanderkiliku@gmail.com>
2019-07-17 11:25:44 +0000

    add action types

---
+++ src/store/action.types.js
@@ -0,0 +1,11 @@
+FETCH_POST="fetchPost"
+FETCH_COMMENTS="fetchComments"
+FETCH_POSTS="fetchPosts"
+FETCH_USER="fetchUser"
+
+export {
+    FETCH_POST,
+    FETCH_POSTS,
+    FETCH_COMMENTS,
+    FETCH_USER
+}
