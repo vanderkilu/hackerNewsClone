9b80689152610f7b2da77217befec790308ad58e
vanderkilu <vanderkiliku@gmail.com>
2019-07-18 12:59:15 +0000

    add user module to store

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import posts from './posts.module'
 import post from './post.module'
+import user from './user.module'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
         posts,
-        post
+        post,
+        user
     }
 })
